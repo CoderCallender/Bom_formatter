0520b6bd831693e0e56a66ca52cff3cdbfe005b9
Chase <chase.callender@limbsandthings.com>
2022-05-25 16:39:26 +0100

    Extra functions

--- dataframe_to_json.py
+++ dataframe_to_json.py
@@ -3,6 +3,8 @@ import simplejson as json
 import pandas as pd
 import string
 import sys
+import xlsxwriter
+from itertools import chain
 
 #returns a new dictionary that only has the values we want (described by the list "wanted_data")
 #dict_to_csv(working_data)  #example dump to csv
@@ -38,6 +40,17 @@ def dict_to_json(dict):
     with open(path, "w") as outfile:
         json.dump(dict, outfile, indent=4, ignore_nan=True)
 
+def check_farnell_stock(dict):
+    for key in dict:
+        #print(dict[key]["OC_FARNELL"])
+        if(type(dict[key]["OC_FARNELL"]) is str):
+            print("hello")
+            dict[key] = "FARNELL STOCK"
+           # dict[key]["FARNELL STOCK"] = "checked"
+            print(dict[key])
+    
+    return(dict)
+
 
 
 #return a list of all the values for a given item request
@@ -60,8 +73,6 @@ def get_all_values(dict, item_to_list):
     return list
 
 
-
-
 #accepts the header to be searched and the term to look for
 #returns the indexs where the search term is found
 #example:
@@ -79,7 +90,7 @@ def get_index(dict, search_header, search_term):
             #print(key)
     return list
 
-#get all the entries in a given header that is empty
+#get all the entries in a given header that are empty
 def get_empty_entries(dict, search_header):
         #create list
     list = []
@@ -89,7 +100,7 @@ def get_empty_entries(dict, search_header):
      #   print(type(dict[key][search_header]))
      if(type(dict[key][search_header]) is not str):
             list.append(key)
-            print(key)
+           # print(key)
     return list
 
 #prints all the values for a given list of indexes
@@ -100,13 +111,79 @@ def print_list(dict, list):
         print(dict[list[x]])
         x += 1
 
+#return a new dictionary that has a particular list that we want
+def index_to_row(dict, list):
+
+    new_dictionary = {}
+    x = 0
+    for item in list:
+        new_dictionary[x] = dict[list[x]]
+       # print(new_dictionary[x])
+        x += 1    
+
+    return new_dictionary
+
+#outputs a list to excel file
+def list_to_xlsx_sheet(list):
+
+    path = sys.path[0] + "\\test.xlsx"
+    df = pd.DataFrame(list)
+
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='list')
+    df.to_excel(writer, sheet_name='list2') #example of saving a new sheet. Must all be done at the same time
+    writer.save()
+
+#outputs a dictionary to excel file
+def dict_to_xlsx(dict):
+
+    path = sys.path[0] + "\\test.xlsx"
+    df = pd.DataFrame.from_dict(dict, orient="index")
+
+    writer = pd.ExcelWriter(path, engine='xlsxwriter')
+    df.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+#searches through a dictionary for duplicates under a given header (eg: duplicate_farnell_numbers = search_duplicates(working_data, "OC_FARNELL"))
+def search_duplicates(dict, search_header):
+
+    simple_dict = {}
+    output_dict = {}
+    #make a simple dictionary of the values
+    for key in dict:
+       # print(dict[key]["Value"])
+        simple_dict[key] = str(dict[key][search_header]).lower()  #creates a dictionary of only the "search_header" values (simplifies the depth of the data)
+        
+    #groups the dictionary by the value istead of the key, which then puts duplicates together
+    rev_dict = {}
+    for key, value in simple_dict.items():
+        rev_dict.setdefault(value, set()).add(key)
+    #print(rev_dict)
+
+    #save the duplicate keys into a new dictionary
+    for key, values in rev_dict.items():
+        if (len(values) > 1):
+            #print(rev_dict[key])
+            output_dict[key] = rev_dict[key]
+
+    print(output_dict)
 
 path = sys.path[0] + "\output.csv"  #create file path from location this program is in
 data_frame =pd.read_csv(path)       #read in the csv file
+#data_frame = data_frame.where(pd.notnull(data_frame), none)        #replace NaN with none
 raw_dictionary = data_frame.to_dict(orient='index') #keep the data as a dictionary
 
 
 working_data = clean_dictionary(raw_dictionary) #get rid of the data we do not want
 
-empty_mpn_list = get_empty_entries(working_data, "MPN")  #get all empty MPN entries
-print_list(working_data,empty_mpn_list)
+#empty_mpn_list = get_empty_entries(working_data, "MPN")  #get all empty MPN entries
+#print_list(working_data,empty_mpn_list)
+
+#empty_manufacturers = index_to_row(working_data , empty_mpn_list)
+#search_duplicates(working_data, "OC_FARNELL")
+#dict_to_xlsx(empty_manufacturers)
+#dict_to_json(empty_manufacturers)
+#dict_to_xlsx(working_data)
+#list_to_xlsx_sheet(empty_mpn_list)
+working_data = check_farnell_stock(working_data)
+dict_to_xlsx(working_data)
